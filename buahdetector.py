171c2416174fff33305a6760a847642dd36ac80c
Riefky Arif I <80331973+riefkyarif@users.noreply.github.com>
2021-06-04 00:19:08 +0700

    Update buahdetector.py

--- buahdetector.py
+++ buahdetector.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""buahdetect4.ipynb
-
-# Import Data from Kaggle
-"""
-
-
 """# Modelling
 
 > Setup libraries & imports
@@ -210,30 +203,3 @@ tflite_model = converter.convert()
 
 with open('/content/drive/MyDrive/Project/Fruit/tflitefruit/modelFruit11.tflite', 'wb') as f:
   f.write(tflite_model)
-
-"""# Test the model accuracy from input (fruit object)"""
-
-from PIL import Image
-from keras.preprocessing import image
-
-#preprocess file
-def prepare_img(path):
-    img = image.load_img(path,target_size=(100,100))
-    img_arr = image.img_to_array(img)
-    img_arr_expnd  = np.expand_dims(img_arr,axis=0)
-    img = keras.applications.mobilenet_v2.preprocess_input(img_arr_expnd)
-    
-    return img
-
-labels = list(test_data.class_indices.keys())
-
-labels
-
-testImg = plt.imshow(Image.open('/content/my_data/fruits-360/Test/Walnut/85_100.jpg'));
-
-prep_img1 = prepare_img('/content/my_data/fruits-360/Test/Walnut/85_100.jpg')
-prep_img1.shape
-
-pred1 = model.predict(prep_img1)
-
-labels[np.argmax(pred1)]
